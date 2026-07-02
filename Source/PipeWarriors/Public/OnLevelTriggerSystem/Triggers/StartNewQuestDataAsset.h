79a124f783a7fdcd1c8fc3fddb3ce75c77027423
Ravadsky <ravadsky@gmail.com>
2026-07-02 13:46:16 +0300

    quest classes created

---
+++ Source/PipeWarriors/Public/OnLevelTriggerSystem/Triggers/StartNewQuestDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnLevelTriggerSystem/OnLevelTriggerDataAsset.h"
+#include "StartNewQuestDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIPEWARRIORS_API UStartNewQuestDataAsset : public UOnLevelTriggerDataAsset
+{
+	GENERATED_BODY()
+	
+};
